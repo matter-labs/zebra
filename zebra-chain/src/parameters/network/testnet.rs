0353539530b5427ed67bf672121012bb15e6da99
Igor Aleksanov <popzxc@yandex.ru>
2025-12-10 16:47:40 +0400

    Working prototype

--- zebra-chain/src/parameters/network/testnet.rs
+++ zebra-chain/src/parameters/network/testnet.rs
@@ -53,7 +53,7 @@ const TESTNET_GENESIS_HASH: &str =
 
 /// The halving height interval in the regtest is 6 hours.
 /// [zcashd regtest halving interval](https://github.com/zcash/zcash/blob/v5.10.0/src/consensus/params.h#L252)
-const PRE_BLOSSOM_REGTEST_HALVING_INTERVAL: HeightDiff = 144;
+const PRE_BLOSSOM_REGTEST_HALVING_INTERVAL: HeightDiff = 100_000; // TODO: Virtually isable halving for testing
 
 /// Configurable funding stream recipient for configured Testnets.
 #[derive(Serialize, Deserialize, Clone, Debug)]
